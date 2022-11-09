7a90971f07f3188830d263a8497001676f2b77b5
Abdul <117847059+Abdulhaydev@users.noreply.github.com>
2022-11-09 22:11:31 +0500

    Create my_new_year.py

---
+++ my_new_year.py
@@ -0,0 +1,10 @@
+"""faqat ko'ngilochar dastur"""
+
+ Yangi_yil sinfi :
+    def  new_years_tree ():
+        
+         i  uchun ( 1 ,  12 ) diapazonida :
+            chop etish ( " "  * ( 12  -  i ), i  *  "*"  +  i  *  "*" )
+        chop etish ( "Yangi yilingiz bilan!!!" )
+
+Yangi_yil . yangi_yillar_daragi ()
